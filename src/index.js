0a9453473fb8a80f80065f2f59183cf29aa2aeaa
ashukumar2021 <86044488+ashukumar2021@users.noreply.github.com>
2021-07-18 20:12:49 +0530

    persistance added to app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import "./index.css";
 import App from "./App";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,16 @@
 import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import notes from "./reducers/notes";
 
 const rootReducer = combineReducers({ notes });
-export const store = createStore(rootReducer);
-console.log(store.getState());
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
+
+export { store, persistor };
