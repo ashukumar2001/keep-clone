c69ec1abc9e430ef003b74b7e2b2b4c578d2d3c3
ashukumar2021 <86044488+ashukumar2021@users.noreply.github.com>
2021-09-17 19:46:40 +0530

    pwa

---
+++ public/serviceworker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "keep-clone-v1.0.0";
+const urlsToCache = ["index.html", "../src/index.js", "offline.html"];
+
+const self = this;
+
+// Installation
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// Activate the Service Worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
